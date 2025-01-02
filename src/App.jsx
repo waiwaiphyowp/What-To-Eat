33e46a128b611b61f3e7140483197eec5a87faa1
wai wai <waiwaiphyochun@gmail.com>
2025-01-02 18:51:58 +0800

    add for searching

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Favorite from './components/Favorite/Favorite'
@@ -28,5 +27,3 @@ const App = () => {
 }
 
 export default App;
-
-  
