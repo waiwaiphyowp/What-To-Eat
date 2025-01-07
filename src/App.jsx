f201af6133ca255bf387586452f437199734b873
wai wai <waiwaiphyochun@gmail.com>
2025-01-07 21:01:56 +0800

    update mealCategory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Favorite from './components/Favorite/Favorite'
+import FavoriteBtn from './components/FavoriteBtn/FavoriteBtn';
 import Home from './components/Home/Home'
 import NavBar from './components/NavBar/NavBar'
 import Search from './components/Search/Search'
@@ -17,7 +17,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/Search" element={<Search />} />
             <Route path="/MealCategory" element={<MealCategory />} /> 
-            <Route path="/Favorite" element={<Favorite />} />
+            <Route path="/Favorite" element={<FavoriteBtn />} />
           </Routes>
         </Router>
       </div>
